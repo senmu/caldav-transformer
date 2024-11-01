d7fec58de0135e14992076cfd32af91bd520a859
Sam Lu <sam.lu@yahooinc.com>
2024-11-01 11:49:12 -0600

    Request CAL_URL and send contents to res

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const request = new Request(process.env.CAL_URL);
+
+  (async () => {
+     let response = await fetch(request)
+     res.status(response.status)
+     if(response.ok) {
+     	for (const entry of response.headers.entries()) {
+     		res.set(entry[0], entry[1])
+     	}
+        response = await response.text()
+        res.send(response)
+     } else {
+        console.error("Response received, but NOT OK")
+     }
+  })().catch(err => {
+      console.error(err)
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
